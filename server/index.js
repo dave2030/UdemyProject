a0b70796c2b3faf77311f270fad05d32864b0d0a
dave2030 <52425817+dave2030@users.noreply.github.com>
2023-02-12 11:54:50 -0500

    Updated project files for deployment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 app.get('/',(req,res)=>{
-    res.send({"hi":'there'});
+    res.send({hi:'there'});
 });
+const PORT=process.env.PORT||5000;//process.env is for production purposes (port provided by deployment tool) 
 app.listen(5000);
