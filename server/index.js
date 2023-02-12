7992e62b9b70d7135db2b8e2206cc53cf85749cc
dave2030 <52425817+dave2030@users.noreply.github.com>
2023-02-12 12:42:27 -0500

    Updated

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
-const express=require('express');
-const app=express();
-app.get('/',(req,res)=>{
-    res.send({hi:'there'});
-});
-const PORT=process.env.PORT||5000;//process.env is for production purposes (port provided by deployment tool) 
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const app = express();
+
+passport.use(new GoogleStrategy());
+
+const PORT = process.env.PORT || 5000; //process.env is for production purposes (port provided by deployment tool)
 app.listen(5000);
